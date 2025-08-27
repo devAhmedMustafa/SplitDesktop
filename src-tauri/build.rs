5a705d211e40763ef9eedb8c1f270ad2e02191e1
Ahmed Mustafa <am1804067@gmail.com>
2025-08-27 19:05:18 +0300

    Windows runtime

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,9 @@
 fn main() {
 
     if cfg!(target_os = "windows") {
+
+        println!("cargo:rustc-link-arg=/NODEFAULTLIB:LIBCMTD");
+        println!("cargo:rustc-link-lib=msvcrtd");
     
         println!("cargo:rustc-link-search=native=static");
         println!("cargo:rustc-link-lib=static=scm_bindings");
